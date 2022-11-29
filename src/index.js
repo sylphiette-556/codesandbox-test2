840047a53c975575bc0ba1c2b503f810a43e7549
sylphiette-556 <118509475+sylphiette-556@users.noreply.github.com>
2022-11-30 00:05:45 +0900

    論理演算子の本当の意味を知った なるほどなあ

--- src/index.js
+++ src/index.js
@@ -29,8 +29,42 @@ document.getElementById("app").innerHTML = `
 //   }
 // })
 
-const val1 = 1 > 0 ? "trueです" : "falseです";
-console.log(val1);
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
 
-const num = 1300;
-console.log(num.toLocaleString());
+// const num = 1300;
+// // console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "アウトやて";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "アウト" : "範囲内";
+// };
+// console.log(checkSum(50, 60));
+
+/**
+ * 論理演算子の本当の意味を知ろう　&& ||
+ */
+
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1か2もtrueになります");
+// }
+
+// ||は、左側がfalseなら右側の値を返す
+// const num = null; // nullはfalse
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は、左側がtrueなら右側を返す
+const num2 = 100;
+const fee2 = num2 && "何か設定された";
+console.log(fee2);
