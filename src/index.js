a3418c3200afa1322d30d679100b0bc5d928723e
sylphiette-556 <118509475+sylphiette-556@users.noreply.github.com>
2022-11-29 23:12:25 +0900

    toLocaleString()すげえや 1000を1,000にするやつ

--- src/index.js
+++ src/index.js
@@ -8,3 +8,29 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+
+// const nameArr = ["田中", "山田", "じゃけえ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => name);
+// console.log(nameArr2);
+
+// nameArr.map((name) => console.log(name));
+
+// const nameArr3 = nameArr.filter((name) => {
+//   return name==='山田'
+// })
+// console.log(nameArr3)
+// nameArr.map((name, index) => {
+//   if (index % 2 === 0) {
+//     console.log(name +2);
+//   }
+// })
+
+const val1 = 1 > 0 ? "trueです" : "falseです";
+console.log(val1);
+
+const num = 1300;
+console.log(num.toLocaleString());
